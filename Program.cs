906492f8b945495bbc6f2121dafd4563d5c7c58e
Tus1688 <76937231+Tus1688@users.noreply.github.com>
2021-12-11 13:19:04 +0700

    Feat(menu): system threading + bird animation, still missing default animation (#1)
    * feat(menu): system threading + ascii bird, still
    missing defualt animations
    
    * fix(Program.cs): whitespace linter

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine();
+        Menu menu = new Menu();
+
+        while (menu.option != MenuOption.Quit)
+        {
+            menu.SpawnMenu();
+        }
     }
 }
