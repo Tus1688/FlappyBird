dc7c909c101cd122b3b5e493f4906562abf1e099
Tus1688 <76937231+Tus1688@users.noreply.github.com>
2021-12-16 17:16:56 +0700

    Feat(core): pipe split without game check, still missing Initial render + countdown (#3)
    * feat(core): pipe is working
    
    * fix(core): obstacle bug
    
    * feat(core): game input & jump
    
    * fix(core): initial render
    
    * fix(core): pipe split width

--- Game.cs
+++ Game.cs
@@ -4,6 +4,7 @@ class Game
 {
     public Game() { } // Make game callable
 
+    Random rand = new Random();
     ConsoleKeyInfo cki;
 
     /*
@@ -42,7 +43,7 @@ class Game
     int splitStart2, splitLength2, pipePivotX2;
 
     int pipeWidth, extraRender;  // extrarender prevent jitter from pipe gone away
-    bool restart, isFlying, gameOver;
+    bool restart, isFlying, gameOver, isPrinted;
 
     Menu menu = new Menu();
 
@@ -51,16 +52,88 @@ class Game
         bird[0, 0] = ' '; bird[0, 1] = ','; bird[0, 2] = '-'; bird[0, 3] = '.'; bird[0, 4] = ' ';
         bird[1, 0] = '>'; bird[1, 1] = wing; bird[1, 2] = ' '; bird[1, 3] = eye; bird[1, 4] = '>';
         bird[2, 0] = ' '; bird[2, 1] = '*'; bird[2, 2] = '%'; bird[2, 3] = '*'; bird[2, 4] = ' ';
+
+        birdX[0, 0] = pivotX - 2; birdY[0, 0] = pivotY - 1;
+        birdX[0, 1] = pivotX - 1; birdY[0, 1] = pivotY - 1;
+        birdX[0, 2] = pivotX; birdY[0, 2] = pivotY - 1;
+        birdX[0, 3] = pivotX + 1; birdY[0, 3] = pivotY - 1;
+        birdX[0, 4] = pivotX + 2; birdY[0, 4] = pivotY - 1;
+
+        birdX[1, 0] = pivotX - 2; birdY[1, 0] = pivotY;
+        birdX[1, 1] = pivotX - 1; birdY[1, 1] = pivotY;
+        birdX[1, 2] = pivotX; birdY[1, 2] = pivotY;
+        birdX[1, 3] = pivotX + 1; birdY[1, 3] = pivotY;
+        birdX[1, 4] = pivotX + 2; birdY[1, 4] = pivotY;
+
+        birdX[2, 0] = pivotX - 2; birdY[2, 0] = pivotY + 1;
+        birdX[2, 1] = pivotX - 1; birdY[2, 1] = pivotY + 1;
+        birdX[2, 2] = pivotX; birdY[2, 2] = pivotY + 1;
+        birdX[2, 3] = pivotX + 1; birdY[2, 3] = pivotY + 1;
+        birdX[2, 4] = pivotX + 2; birdY[2, 4] = pivotY + 1;
     }
 
     private void Pipe1()
     {
-
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < pipeWidth; j++)
+            {
+                if (j < extraRender)
+                {
+                    pipeX[i, j] = pipePivotX - (extraRender - j);
+                }
+                else if (j > extraRender)
+                {
+                    pipeX[i, j] = pipePivotX + (j - extraRender);
+                }
+                else if (j == extraRender)
+                {
+                    pipeX[i, j] = pipePivotX;
+                }
+                pipeY[i, j] = i;
+                pipe[i, j] = '1'; // pipe1 char
+            }
+        }
+        for (int k = splitStart; k < splitLength + splitStart; k++)
+        {
+            for (int l = 0; l < pipeWidth; l++)
+            {
+                pipe[k, l] = ' ';
+            }
+        }
+        Thread.Sleep(30); // most important thing
     }
 
     private void Pipe2()
     {
-
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < pipeWidth; j++)
+            {
+                if (j < extraRender)
+                {
+                    pipeX2[i, j] = pipePivotX2 - (extraRender - j);
+                }
+                else if (j > extraRender)
+                {
+                    pipeX2[i, j] = pipePivotX2 + (j - extraRender);
+                }
+                else if (j == extraRender)
+                {
+                    pipeX2[i, j] = pipePivotX2;
+                }
+                pipeY2[i, j] = i;
+                pipe2[i, j] = '2'; // pipe2 char
+            }
+        }
+        for (int k = splitStart2; k < splitLength2 + splitStart2; k++)
+        {
+            for (int l = 0; l < pipeWidth; l++)
+            {
+                pipe2[k, l] = ' ';
+            }
+        }
+        Thread.Sleep(30); // most important thing
     }
 
     private void CountDown()
@@ -117,4 +190,260 @@ class Game
         }
     }
 
+    private void Setup()
+    {
+        height = 30;
+        width = 60;
+
+        score = 0;
+        fallDelay = 1;
+        upwardSpeed = 3;
+        fallSpeed = 1;
+        wing = 'v';
+
+        gameOver = false;
+        restart = false;
+        isFlying = false;
+
+        pivotX = 25;
+        pivotY = height / 2;
+
+        splitStart = rand.Next(5, height - 10);
+        splitStart2 = rand.Next(3, height - 11);
+        splitLength = splitLength2 = 9;
+        pipePivotX = 60;
+        pipePivotX2 = pipePivotX + pipeWidth + 22;
+        pipeWidth = 15;
+        extraRender = pipeWidth / 2;
+    }
+
+    private void InitialRender()
+    {
+        Console.Clear();
+        pivotX = 30;
+        pivotY = 10;
+        Bird('v', 'o');
+
+        Console.WriteLine("================================");
+        Console.WriteLine("supert duper bird");
+        Console.WriteLine("================================");
+        for (int i = 6; i < 14; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                isPrinted = false;
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int l = 0; l < 5; l++)
+                    {
+                        if (j == birdX[k, l] && i == birdY[k, l])
+                        {
+                            if (j == pivotX + 1 && i == pivotY)
+                            {
+                                Console.Write(bird[k, l]);
+                            }
+                            else if (j == pivotX - 1 && i == pivotY)
+                            {
+                                Console.Write(bird[k, l]);
+                            }
+                            else if (j == pivotX + 2 && i == pivotY)
+                            {
+                                Console.Write(bird[k, l]);
+                            }
+                            else
+                            {
+                                Console.Write(bird[k, l]);
+                            }
+                            isPrinted = true;
+                        }
+                    }
+                }
+                if (!isPrinted)
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+        Console.WriteLine();
+    }
+
+    private void Render()
+    {
+        if (!gameOver)
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    isPrinted = false;
+                    for (int k = 0; k < 3; k++)
+                    {
+                        for (int l = 0; l < 5; l++)
+                        {
+                            if (j == birdX[k, l] && i == birdY[k, l])
+                            {
+                                if (j == pivotX + 1 && i == pivotY)
+                                {
+                                    if (bird[k, l] == 'o')   // bird eye alive? color white : color red;
+                                    {
+                                        Console.ForegroundColor = ConsoleColor.White;
+                                    }
+                                    else
+                                    {
+                                        Console.ForegroundColor = ConsoleColor.Red;
+                                    }
+                                }
+                                else if (j == pivotX - 1 && i == pivotY)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                }
+                                else if (j == pivotX + 2 && i == pivotY)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                }
+                                Console.Write(bird[k, l]);
+                                isPrinted = true;
+                            }
+                        }
+                    }
+                    if (!isPrinted)
+                    {
+                        for (int x = 0; x < height; x++)
+                        {
+                            for (int y = 0; y < pipeWidth; y++)
+                            {
+                                if (j == pipeX[x, y] && i == pipeY[x, y])
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.Write(pipe[x, y]);
+                                    isPrinted = true;
+                                }
+                            }
+                        }
+                    }
+                    if (!isPrinted)
+                    {
+                        for (int a = 0; a < height; a++)
+                        {
+                            for (int b = 0; b < pipeWidth; b++)
+                            {
+                                if (j == pipeX2[a, b] && i == pipeY2[a, b])
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.Write(pipe2[a, b]);
+                                    isPrinted = true;
+                                }
+                            }
+                        }
+                    }
+                    if (!isPrinted)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.SetCursorPosition(0, height);
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine($"Your score: {score}");
+        }
+    }
+    private void GameInput()
+    {
+        while (Console.KeyAvailable)
+        {
+            if (!gameOver)
+            {
+                cki = Console.ReadKey(true);
+            }
+            if (cki.Key == ConsoleKey.Spacebar)
+            {
+                isFlying = true;
+            }
+        }
+    }
+
+    private void Logic()
+    {
+        pipePivotX--;
+        pipePivotX2--;
+        fallDelay++;
+        wingDelay++;
+
+        if (wingDelay == 1)
+        {
+            wing = '^';
+        }
+        if (fallDelay == 1)
+        {
+            pivotY += fallSpeed;
+            fallDelay = 0;
+        }
+        if (isFlying)
+        {
+            pivotY -= upwardSpeed;
+            wing = 'v';
+            fallDelay = -1;
+            wingDelay = -1;
+            isFlying = false;
+        }
+        if (pipePivotX == pivotX - extraRender || pipePivotX2 == pivotX - extraRender)
+        {
+            score++;
+            if (score > highScore)
+            {
+                highScore = score;
+            }
+        }
+        if (pipePivotX == -extraRender)
+        {
+            pipePivotX = width + extraRender;
+            splitStart = rand.Next(3, height - splitLength - 5);
+        }
+        if (pipePivotX2 == -extraRender)
+        {
+            pipePivotX2 = width + extraRender;
+            splitStart2 = rand.Next(3, height - 13);
+        }
+        Pipe1();
+        Pipe2();
+
+        Bird(wing, 'o');
+    }
+
+    private void UpdateState()
+    {
+        Console.Clear();
+        while (true)
+        {
+            GameInput();
+            Logic();
+            Render();
+            Thread.Sleep(10);
+            if (gameOver || restart)
+            {
+                break;
+            }
+        }
+    }
+
+    public void LoadGame()
+    {
+        CountDown();
+        Setup();
+        UpdateState();
+    }
+
+    public void InitialLoad()
+    {
+        Setup();
+        LoadGame();
+    }
 }
--- Program.cs
+++ Program.cs
@@ -5,10 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Menu menu = new Menu();
+        Game game = new Game();
 
-        while (menu.option != MenuOption.Quit)
-        {
-            menu.SpawnMenu();
-        }
+        game.InitialLoad();
+        /*
+         *while (menu.option != MenuOption.Quit)
+         *{
+         *    menu.SpawnMenu();
+         *    if (menu.option == MenuOption.Start)
+         *    {
+         *        game.LoadGame();
+         *    }
+         *}
+         */
     }
 }
